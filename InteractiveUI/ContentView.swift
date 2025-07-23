6a36ea0708c4e5bdc5b4f4e1ffd6df0a16fc6d3a
Diana <diudiana7788@gmail.com>
2025-07-23 14:14:29 -0700

    added action for button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,21 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //@State allows the variable to be updated
+    //$ is a two-way-binding & it allows name to be immediatedly updated
     @State private var name: String = ""
     @State private var textTitle = "What is your name?"
 
     var body: some View {
         VStack {
             
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
             TextField("Type your name here", text: $name)
                 .padding(.horizontal)
                 .multilineTextAlignment(.center)
                 .border(Color.gray, width:1)
             Button("Submit Name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
+                textTitle = "Welcome, \(name)!"            }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
